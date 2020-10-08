e2eb520c078fa9e5666efaa3ae43038f7d013264
leonarDubois <lha.dubois@gmail.com>
2020-10-08 11:40:06 +0000

    move settings

--- settings.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Aitor Blanco (aitor.blancomiguez@unitn.it)'
-__version__ = '0.01'
-__date__ = '12 Aug 2020'
-
-
-MMSEQ2_PATH = "/shares/CIBIO-Storage/CM/mir/tools/MMseqs2/build/bin/"
-#MMSEQ2_PATH = "/shares/CIBIO-Storage/CM/mir/tools/MMseqs2/build_cm3/bin/"
-DIAMOND_PATH = '/shares/CIBIO-Storage/CM/mir/tools/diamond-0.9.24/diamond'
-BOWTIE2_PATH = "/shares/CIBIO-Storage/CM/mir/tools/bowtie2-2.3.4.3/"
